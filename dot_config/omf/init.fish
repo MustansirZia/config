2485ff570102f127805da2b3f8aa36931fe54240
Mustansir Zia <mustansir@Mustansirs-MacBook-Air.local>
2024-03-26 15:28:42 +0530

    update: init.fish

--- dot_config/omf/init.fish
+++ dot_config/omf/init.fish
@@ -5,14 +5,16 @@ set -gx ANDROID_HOME $HOME/Library/Android/sdk
 set -gx LC_ALL en_US.UTF-8
 set -gx LANG en_US.UTF-8
 set -gx N_PREFIX $HOME/.n
+set -gx HOMEBREW_PATH /opt/homebrew
 
 fish_add_path $N_PREFIX/bin:
 fish_add_path $WORKSPACE_PATH/bin
 fish_add_path $ANDROID_HOME/platform-tools
 fish_add_path $ANDROID_HOME/build-tools/28.0.3
 fish_add_path $ANDROID_HOME/tools
-fish_add_path /opt/homebrew/Cellar/postgresql@11/11.20_1/bin
-fish_add_path /opt/homebrew/opt/python@3.11/libexec/bin
+fish_add_path $HOMEBREW_PATH/bin/
+fish_add_path $HOMEBREW_PATH/Cellar/postgresql@11/11.20_1/bin
+fish_add_path $HOMEBREW_PATH/opt/python@3.11/libexec/bin
 fish_add_path /usr/local/Cellar/ruby/2.7.0/bin/
 
 # 2) Set aliases.
