bb4f53891ef6f4f8d9bea73a3b342c5e0162c1f6
MustansirZia <mustansir29@hotmail.com>
2024-06-08 15:20:38 +0530

    chore: remove single quotes from init.fish

--- dot_config/omf/init.fish
+++ dot_config/omf/init.fish
@@ -19,24 +19,24 @@ fish_add_path /usr/local/Cellar/ruby/2.7.0/bin/
 
 # 2) Set aliases.
 # git aliases.
-alias gp="git push" 
-alias gl="git pull"
-alias gc="git add . && git commit -m" 
-alias gs="git stash"
-alias gk="git checkout"
+alias gp='git push' 
+alias gl='git pull'
+alias gc='git add . && git commit -m' 
+alias gs='git stash'
+alias gk='git checkout'
 # rosetta aliases.
-alias x86="arch -x86_64"
+alias x86='arch -x86_64'
 # misc aliases
-alias postgre="postgres -D ~/Workspace/bin/postgres/data"
+alias postgre='postgres -D ~/Workspace/bin/postgres/data'
 # aws vault aliases
-alias qa="aws-vault exec qa --"
+alias qa='aws-vault exec qa --'
 # Turbokat aliases
 function enter
   code $WORKSPACE_PATH/ClearGlass/$argv
 end
-alias base="docker exec -u root -it  $(docker ps -f name=base  --format "{{.ID}}") php artisan --"
-alias tlog="turbokat log"
-alias release="release.sh"
+alias base='docker exec -u root -it  $(docker ps -f name=base  --format "{{.ID}}") php artisan --'
+alias tlog='turbokat log'
+alias release='release.sh'
 
 # 3) iTerm integration.
 set ITERM_INTEGRATION_FILE ~/.iterm2_shell_integration.fish 
